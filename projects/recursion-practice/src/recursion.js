ac8a2676eb468b6bdb0f3f62f540c79f7bde1851
RobertWKnapp <4609saratoga@gmail.com>
2024-10-12 17:51:07 -0500

    6

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -67,16 +67,22 @@ var sumBelow = function(n) {
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
 var range = function(x, y, output = []) {
-    // Base case: If x is greater than or equal to y, return the output array
-    if (x >= y) {
-      return output;
+    // Determine if we need to count up or down
+    if (x < y) {
+      // Base case for counting up
+      if (x >= y - 1) {
+          return output;
+      }
+      output.push(x + 1);
+      return range(x + 1, y, output);
+  } else {
+      // Base case for counting down
+      if (x <= y + 1) {
+          return output;
+      }
+      output.push(x - 1);
+      return range(x - 1, y, output);
   }
-
-  // Add the next integer to the output array
-  output.push(x + 1);
-
-  // Call the function recursively with the next integer
-  return range(x + 1, y, output);
 };
 
 // 7. Compute the exponent of a number.
